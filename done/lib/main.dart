56ffe38d7304d87d6ad4a63208ceb80464f45182
Ahmet Rıdvan Ordulu <Ahmetridvanordulu@outlook.com>
2023-08-23 17:12:47 +0300

    as
    as

--- done/lib/main.dart
+++ done/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//11:55
+// 50 Dakika kaldı.
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,7 +12,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String sehir = "Van";
+
+  void degistir(String yeniSehir){
+      sehir = yeniSehir;
+      setState(() {
+
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +40,7 @@ class MyHomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(child: SolWidgetA()),
-            Expanded(child: SagWidgetA())
+            Expanded(child: SagWidgetA(sehir: sehir,callBack: degistir,))
           ],
         ),
       ),
@@ -35,6 +49,7 @@ class MyHomePage extends StatelessWidget {
 }
 
 class SolWidgetA extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,22 +70,34 @@ class SolWidgetA extends StatelessWidget {
 }
 
 class SagWidgetA extends StatelessWidget {
+
+  String? sehir;
+  Function? callBack;
+
+  SagWidgetA({Key? key,required this.sehir,required this.callBack}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
       child: Column(children: [
         Text(
-          'SagWidget A',
+          'SagWidget A $sehir',
           style: TextStyle(fontSize: 20),
         ),
-        SagWidgetB()
+        SagWidgetB(sehir: sehir,geriArama: callBack,)
       ]),
     );
   }
 }
 
 class SagWidgetB extends StatelessWidget {
+
+  String? sehir;
+  Function? geriArama;
+
+  SagWidgetB({Key? key,required this.sehir,required this.geriArama}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -79,16 +106,22 @@ class SagWidgetB extends StatelessWidget {
       color: Colors.purple,
       child: Column(children: [
         Text(
-          'SagWidget B',
+          'SagWidget B $sehir',
           style: TextStyle(fontSize: 20),
         ),
-        SagWidgetC()
+        SagWidgetC(metin: sehir,gelenFonksiyon: geriArama,)
       ]),
     );
   }
 }
 
 class SagWidgetC extends StatelessWidget {
+  String? metin;
+  Function? gelenFonksiyon;
+
+
+  SagWidgetC({Key? key,required this.metin,required this.gelenFonksiyon}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -97,14 +130,18 @@ class SagWidgetC extends StatelessWidget {
       width: 160,
       child: Column(children: [
         Text(
-          'SagWidget C',
+          'SagWidget C $metin',
           style: TextStyle(fontSize: 20),
         ),
         Text(
-          'Şehir: ... ',
+          'Şehir: ...$metin',
           style: TextStyle(fontSize: 20),
         ),
-        TextField(onChanged: null)
+        TextField(onChanged:
+        (girilen) {
+          gelenFonksiyon!(girilen);
+        },
+        )
       ]),
     );
   }
