0af71e4a3303550a517de36dab57703dc822a2eb
Ahmet Rıdvan Ordulu <Ahmetridvanordulu@outlook.com>
2023-08-30 22:39:25 +0300

    a
    a

--- getitdone/lib/models/İtemsData.dart
+++
@@ -1,9 +0,0 @@
-import 'package:getitdone/models/item.dart';
-
-class ItemData{
-  final List<Item> items = [
-    Item(title: "Cake yap"),
-    Item(title: "Duolingo yap"),
-    Item(title: "İngilizce kitap yap")
-  ];
-}
