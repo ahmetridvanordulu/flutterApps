fb1d875d3e08aaedfc4bc0f4dcf2cb4fb6531b02
Ahmet Rıdvan Ordulu <Ahmetridvanordulu@outlook.com>
2023-09-17 19:19:43 +0300

    ö
    ö

--- quiz/lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color dogruCevap = Color(0xFF2FFF00);
-const Color yanlisCevap = Color(0xffff0000);
-const Color dogalRenk = Color(0xFFFFFFFF);
-const Color arkaPlan = Color(0xFF050095);
---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: StartScreen()), // 10:10
+  ));
+}
