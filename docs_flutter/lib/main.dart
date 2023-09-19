ebac65a3a140bb426c6203420e3cb4f6f646ea49
Ahmet Rıdvan Ordulu <Ahmetridvanordulu@outlook.com>
2023-09-19 14:30:10 +0300

    a
    a

---
+++ docs_flutter/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      debugShowCheckedModeBanner: false,
+      home: bas(),
+    );
+  }
+}
+
+class bas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("merhaba"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          TaskItem(
+            Tasks: "1. Örnek",
+          ),
+          TaskItem(
+            Tasks: "2. Örnek",
+          ),
+          TaskItem(Tasks: "3. Örnek")
+        ],
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+  String Tasks = "";
+
+  TaskItem({super.key, required this.Tasks});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value: false, onChanged: null),
+        Text(Tasks),
+      ],
+    );
+  }
+}
